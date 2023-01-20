4dd34c1102b708426d9cfec3ee3c3a107b14da2e
Olivier Genel <zotri@hotmail.com>
2023-01-20 13:41:09 +0100

    Win system OK

--- views/cricket.php
+++ views/cricket.php
@@ -33,7 +33,7 @@
       constructor(name) {
         this.name = name;
         this.scores = { 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 25: 0 };
-        // the HTML elt that displays player's score, settled with Plateau instance
+        // set the HTML elt that displays player's score, settled with Plateau instance
         this.scoreCell;
       }
 
@@ -48,19 +48,28 @@
           this.updateScore(score)
         }
         // TODO: check if player has won
+        this.checkWin(players);
       }
 
       // Returns true if one cell or more of the row is 'open' (score < 3)
       checkRow(score, players) {
-        let rowScores = Object.values(players)
-                              .map(player => player.scores[score]);
+        let rowScores = players.map(player => player.scores[score]);
         return rowScores.filter(nbr => nbr < 3).length ? true : false
       }
 
       updateScore(score) {
         this.scores[score] += 1;
-        if (this.scores[score] > 3) {
-          this.scoreCell.innerHTML = this.totalScore;
+        if (this.scores[score] > 3) { this.scoreCell.innerHTML = this.totalScore; }
+      }
+
+      checkWin(players) {
+        // check if all cells are closed for player
+        // And if no one has an equal or better totalScore
+        if ((!Object.values(this.scores).filter(nbr => nbr < 3).length) &&
+            (players.map(player => player.totalScore)
+                    .filter(tot => tot >= this.totalScore)
+                    .length == 1)) {
+          alert(`${this.name.toUpperCase()} A GAGNEEEEEE !!!`)
         }
       }
     }
@@ -146,6 +155,7 @@
           target.setAttribute('class', 'temp');
           target.setAttribute('data-img', 'target');
           e.target.replaceChildren(target);
+          player.checkWin(players);
         } else {
           player.checkIfScore(score, players);
         }
