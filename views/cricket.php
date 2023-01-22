6f066184bb4da65b69b31f9f44bd981d009d1309
Olivier Genel <zotri@hotmail.com>
2023-01-22 17:44:11 +0100

    Changed cricket_test.scss to cricket.scss

--- views/cricket.php
+++ views/cricket.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="../assets/cricket_test.css" rel="stylesheet">
+  <link href="../assets/cricket.css" rel="stylesheet">
   <title>Cricket</title>
 
   <?php
