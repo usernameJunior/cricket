546f43f10b354f10d82c431a7eb28036d2ba74ae
Olivier Genel <zotri@hotmail.com>
2023-01-24 12:50:12 +0100

    Removed game choice page, as there will be only cricket for now

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewporindext" content="width=device-width, initial-scale=1.0">
-  <link href="../assets/homepage.css" rel="stylesheet">
+  <!-- favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/icons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/icons/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+
+  <link href="../assets/style.css" rel="stylesheet">
 
   <?php require 'php/players_array_for_js.php' ?>
 
@@ -18,7 +24,9 @@
 </head>
 
 <body>
-  <h1>Joueurs</h1>
+  <h1>Cricket</h1>
+
+  <h2>Joueurs :</h2>
 
   <form action="php/set_players.php"
         method="post">
